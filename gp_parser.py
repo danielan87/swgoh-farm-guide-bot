e288c585b8cffe970487b0325ecf349c71decba8
Daniel AN <daniel.an92@gmail.com>
2017-11-12 02:35:59 -0500

    fix parser

--- gp_parser.py
+++ gp_parser.py
@@ -13,19 +13,16 @@ if __name__ == "__main__":
     guild_page = browser.get("https://swgoh.gg/g/{}/hogtown-knights/".format(GUILD_ID))
     gp_div = guild_page.soup.find("div", {'class': 'stat-item-value'})
     guild_gp = int(gp_div.contents[0].replace(',', ''))
-    r.sadd("guildgp:values:{}".format(GUILD_ID), guild_gp)
-    r.sadd("guildgp:dates:{}".format(GUILD_ID), today)
+    r.rpush("guildgp:values:{}".format(GUILD_ID), guild_gp)
+    r.rpush("guildgp:dates:{}".format(GUILD_ID), today)
     members_table = guild_page.soup.find("table", {'class': 'table'})
     members_table_tds = members_table.find_all('td')
-    # dic = {}
     temp_name = ''
     for td in members_table_tds:
         if temp_name:
-            r.sadd("membergp:{}:{}".format(GUILD_ID, temp_name), td.contents[0])
-            # dic[temp_name] = td.contents[0]
-            # temp_name = ''
+            r.rpush("membergp:{}:{}".format(GUILD_ID, temp_name), td.contents[0])
+            temp_name = ''
         if 'data-sort-value' in str(td):
             temp_name = td.find('strong').contents[0]
         continue
     browser.close()
-    # print(dic)
